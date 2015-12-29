05686b13a00a0b4cbb986d8dd6ef0580cc757b4c
Taras Romaniv <t.romaniv@gmail.com>
2015-12-29 13:52:59 +0200

    Updates

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
 'use strict';
 
 var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs-extra');
+var _ = require('underscore');
+
+// ----------------------------------------------------------------------------
+
 var app = express();
-
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
-// views is directory for all template files
+// ----------------------------------------------------------------------------
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-    res.render('pages/index');
+// ----------------------------------------------------------------------------
+
+app.all('/', function (req, res) {
+    var result;
+    if (req.body && req.body.login && req.body.password) {
+        var list = fs.readJsonSync('data/people.json');
+        var login = req.body.login.trim();
+        var password = req.body.password.trim();
+        if (list[login] && list[login].password === password) {
+            if (list[login].done === false) {
+                var random = _.chain(list).omit(login).pairs().reject(function (item) {
+                    return item[1].booked === true;
+                }).shuffle().first().value();
+                if (random) {
+                    list[login].done = true;
+                    list[random[0]].booked = true;
+                    fs.writeJsonSync('data/people.json', list, {spaces: 2});
+                    result = 'Тобі випало: <strong>' + random[1].name + '</strong>';
+                }
+            } else {
+                result = 'Ти вже знаєш, що тобі випало :)';
+            }
+        }
+    }
+    res.render('pages/index', {result: result});
 });
+
+// ----------------------------------------------------------------------------
 
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
